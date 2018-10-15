a18d0c9df864f261d0efd2223c2cbda5131a2f2e
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-10-15 16:12:37 -0400

    :rotating_light: tests for second simple method

--- TP2/GraphGeneratorTest.java
+++ TP2/GraphGeneratorTest.java
@@ -43,21 +43,40 @@ class GraphGeneratorTest {
         assertEquals(GraphGenerator.simple(12,66).E(), 66);
         //E5
         Throwable tooManyEdges = assertThrows(IllegalArgumentException.class,() -> {
-            GraphGenerator.simple(5, 11);
+            GraphGenerator.simple(12, 11);
         });
     }
 
     @org.junit.jupiter.api.Test
     /*
-    simple() prend 2 entrées: V et E
+    simple() prend 2 entrées: V et p
     V peut avoir n'importe quel int positif ou nul
-    E doit valoir entre 0 et V*(V-1)/2
+    p doit valoir entre 0 1
     On a donc pour V: min-(-1), min(0), V(1 à 2147483647)
-    pour E: min-(-1), min(0), E(1 à (V*(V-1)/2-1)), max(V*(V-1)/2), max+(1+(V*(V-1)/2))
-    Nous avons donc 6 cas de test : V1, E1, V2E2, V3E3, V3E4, E5
+    pour p: min-(-1.0), min(0.0), p(0.01 à 0.99), max(1.0), max+(1.1)
+    Nous avons donc 6 cas de test : V1, P1, V2P2, V3P3, V3P4, P5
     */
     void simpleVp() {
-
+        //V1
+        Throwable negativeVertex = assertThrows(IllegalArgumentException.class,() -> {
+            GraphGenerator.simple(-1, 0.0);
+        });
+        //P1
+        Throwable negativeProbability = assertThrows(IllegalArgumentException.class,() -> {
+            GraphGenerator.simple(0, -1.0);
+        });
+        //V2P2
+        assertEquals(GraphGenerator.simple(0,0.0).V(), 0);
+        assertEquals(GraphGenerator.simple(0,0.0).E(), 0);
+        //V3P3
+        assertEquals(GraphGenerator.simple(25,0.5).V(), 25);
+        //V3P4
+        assertEquals(GraphGenerator.simple(12,1.0).V(), 12);
+        assertEquals(GraphGenerator.simple(12,1.0).E(), 66);
+        //P5
+        Throwable probabilityTooBig = assertThrows(IllegalArgumentException.class,() -> {
+            GraphGenerator.simple(12, 1.1);
+        });
     }
 
     @org.junit.jupiter.api.Test
