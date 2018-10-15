0ce91c577658b57aa0813cea0d91ca5f5dcc86da
Paul-Arthur THIERY <paul-arthur.thiery@isep.fr>
2018-10-15 13:33:43 -0400

    Empty testing class for Lab 2

---
+++ TP2/Tests/GraphGeneratorTest.java
@@ -0,0 +1,34 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphGeneratorTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void simple() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void simple1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bipartite() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bipartite1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void regular() {
+    }
+
+}
